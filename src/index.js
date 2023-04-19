e25abc654088c9f96014b064c29567285db99071
AlexSystemAdmin <alexkuzne7soff@yandex.ru>
2023-04-19 22:23:40 +0300

    Lesson 1 - 16

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
+import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss'
+
+ // core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    modules: [Navigation, Pagination],
+  });
+
+  var swiper = new Swiper(".trendsSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    modules: [Navigation, Pagination],
+  });
